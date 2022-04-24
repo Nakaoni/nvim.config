69d5ea167ac51c4fb7b8505a2edc9f214ed0a521
Nakaoni <cho_tenro@hotmail.fr>
2022-04-24 18:56:41 +0200

    update cursorline display

--- lua/user/options.lua
+++ lua/user/options.lua
@@ -1,36 +1,35 @@
 -- :help options
 
 local options = {
-    cursorline = false,          -- Cursor line activation
-    number = true,               -- Display line numbers
-    relativenumber = true,       -- Display line numbers relative to cursor position
-    expandtab = true,            -- Usage of spaces to insert a tab in Insert mode
-    softtabstop = 4,             -- Number of spaces for tabs
-    shiftwidth = 4,              -- Number of spaces to use for each step of (auto)indent
-    autoindent = true,           --
-    clipboard = "unnamedplus",   --
-    mouse = "a",                 --
-    ignorecase = true,           --
-    hlsearch = true,             --
-    incsearch = true,            --
-    ruler = true,                --
-    showcmd = true,              --
-    so = 10,                     --
-    fileencoding = "utf-8",      --
-    cmdheight = 2,               --
-    showtabline = 1,             --
-    smartcase = true,            --
-    smartindent = true,          --
-    splitbelow = true,           --
-    splitright = true,           --
-    numberwidth = 2,             -- Width of the number column
-    wrap = false,                --
-    scrolloff = 8,               --
-    sidescrolloff = 8,           --
+    cursorline = true, -- Cursor line activation
+    number = true, -- Display line numbers
+    relativenumber = true, -- Display line numbers relative to cursor position
+    expandtab = true, -- Usage of spaces to insert a tab in Insert mode
+    softtabstop = 4, -- Number of spaces for tabs
+    shiftwidth = 4, -- Number of spaces to use for each step of (auto)indent
+    autoindent = true, --
+    clipboard = "unnamedplus", --
+    mouse = "a", --
+    ignorecase = true, --
+    hlsearch = true, --
+    incsearch = true, --
+    ruler = true, --
+    showcmd = true, --
+    so = 10, --
+    fileencoding = "utf-8", --
+    cmdheight = 2, --
+    showtabline = 1, --
+    smartcase = true, --
+    smartindent = true, --
+    splitbelow = true, --
+    splitright = true, --
+    numberwidth = 2, -- Width of the number column
+    wrap = false, --
+    scrolloff = 8, --
+    sidescrolloff = 8, --
     termguicolors = false,
 }
 
 for key, value in pairs(options) do
     vim.opt[key] = value
 end
-
