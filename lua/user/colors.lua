bde96c1380f2df5f34d9771fe22c177ba72f4481
Nakaoni <cho_tenro@hotmail.fr>
2022-05-11 23:48:13 +0200

    Upgrade Neovim to 0.7 and use a new colorscheme

--- lua/user/colors.lua
+++ lua/user/colors.lua
@@ -1,11 +1,42 @@
-local colorscheme = "darkplus"
-local colorscheme = "vscode"
+-- local colorscheme = "darkplus"
+-- local colorscheme = "vscode"
+-- local colorscheme = "github-colors"
+-- vim.g.vscode_style = "dark"
 
-vim.g.vscode_style = "dark"
+-- -- local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+--
+-- if not status_ok then
+--     vim.notify("Colorscheme " .. colorscheme .. "not installed !")
+-- end
 
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+-------------------------------------------
+-- local colorscheme = "github-theme"
+--
+-- local status_ok, theme = pcall(require, colorscheme)
+-- if not status_ok then
+--     vim.notify("Colorscheme " .. colorscheme .. " not installed !")
+-- end
+--
+-- theme.setup({
+--     theme_style = "dark",
+--     comment_style = "italic",
+--     keyword_style = "NONE",
+--     function_style = "NONE",
+--     variable_style = "NONE",
+--     dark_sidebar = false,
+-- })
+-------------------------------------------
 
+local colorscheme = "rose-pine"
+
+local status_ok, theme = pcall(require, colorscheme)
 if not status_ok then
-    vim.notify("Colorscheme " .. colorscheme .. "not installed !")
+    vim.notify("Colorscheme " .. colorscheme .. " not installed !")
 end
 
+theme.setup({
+    dark_variant = "moon",
+    disable_italics = true,
+})
+
+vim.cmd("colorscheme rose-pine")
